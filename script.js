513f0acf9e1060ec7138d65588b5fbb35001bc6b
Leah Davis <ileahd96@gmail.com>
2021-09-22 22:40:02 -0400

    Added in else if statements and created for loop for selecting random characters

--- script.js
+++ script.js
@@ -8,29 +8,28 @@ var usesNumbers;
 var usesSpecialCharacters;
 var usesUpperCase;
 var usesLowerCase;
+var password;
+var passwordText;
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Add event listener to generate button
+generateBtn.addEventListener("click", function writePassword() {
+      password = generatePassword();
+      passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+});
 
 
 function generatePassword() {
-  passwordLength = prompt("Enter the desired length of your password:");
+  passwordLength = parseInt(prompt("Enter the desired length of your password:"));
   if (!passwordLength) {
-      alert("Please enter a value");
+      alert("Please enter a value.");
   } else if (passwordLength < 8 || passwordLength > 128) {
-      alert("Please choose a length between 8 and 128");
+      parseInt(alert("Please choose a length between 8 and 128"));
   } else { 
       usesNumbers = confirm("Include numbers?");
       usesSpecialCharacters = confirm("Include special characters?");
@@ -38,15 +37,70 @@ function generatePassword() {
       usesLowerCase = confirm("Includes lowercase characters?");
   }
 
-  if (usesNumbers && usesSpecialCharacters && usesUpperCase && usesLowerCase) {
-      selectedChars = uppercaseCharacters.concat(lowercaseCharacters, numbCharacters, specialCharacters);
+  // If no options are chosen...
+  if (!usesSpecialCharacters && !usesNumbers && !usesUpperCase && !usesLowerCase) {
+    selectedChars = alert("You must choose at least one character type.");
   }
+
+  // All selected options
+  else if (usesSpecialCharacters && usesNumbers && usesUpperCase && usesLowerCase) {
+    selectedChars = specialCharacters.concat(numbCharacters, lowercaseCharacters, uppercaseCharacters);
+  }
+
+  // Three selected options
+  else if (usesSpecialCharacters && usesNumbers && usesUpperCase) {
+    selectedChars = specialCharacters.concat(numbCharacters, uppercaseCharacters);
+  }
+  else if (usesSpecialCharacters && usesNumbers && usesLowerCase) {
+    selectedChars = specialCharacters.concat(numbCharacters, lowercaseCharacters);
+  }
+  else if (usesSpecialCharacters && usesLowerCase && usesUpperCase) {
+    selectedChars = specialCharacters.concat(lowercaseCharacters, uppercaseCharacters);
+  }
+  else if (usesNumbers && usesLowerCase && usesUpperCase) {
+    selectedChars = numbCharacters.concat(lowercaseCharacters, uppercaseCharacters);
+  }
+
+  // Two selected options
+  else if (usesSpecialCharacters && usesNumbers) {
+    selectedChars = specialCharacters.concat(numbCharacters);
+  } 
+  else if (usesSpecialCharacters && usesLowerCase) {
+    selectedChars = specialCharacters.concat(lowercaseCharacters);
+  } 
+  else if (usesSpecialCharacters && usesUpperCase) {
+    selectedChars = specialCharacters.concat(uppercaseCharacters);
+  }
+  else if (usesLowerCase && usesNumbers) {
+    selectedChars = lowercaseCharacters.concat(numbCharacters);
+  } 
+  else if (usesLowerCase && usesUpperCase) {
+    selectedChars = lowercaseCharacters.concat(uppercaseCharacters);
+  } 
+  else if (usesNumbers && usesUpperCase) {
+    selectedChars = numbCharacters.concat(uppercaseCharacters);
+  }
+
+  // One selected option
+  else if (usesSpecialCharacters) {
+    selectedChars = specialCharacters;
+  }
+  else if (usesNumbers) {
+    selectedChars = numbCharacters;
+  }
+  else if (usesLowerCase) {
+    selectedChars = usesLowerCase;
+}
+  else if (usesUpperCase) {
+    selectedChars = uppercaseCharacters;
+  }
+
+  
+  var password = [];
 
   for (var i = 0; i < passwordLength; i++) {
-    passwordText += Math.floor(Math.random() * fullCharacterSet.length);
-    passwordText[i]
+    var passwordText = selectedChars[Math.floor(Math.random() * selectedChars.length)];
+    password.push(passwordText);
   }
-}
+} 
 
-// Asks for user input on what types of characters to include
-  
