e78af3dbb4f64696b5fdb6bd6aa8a9b2ebaa0072
Leah Davis <ileahd96@gmail.com>
2021-09-23 16:22:51 -0400

    Updated for loop for password randomization and swapped password variable from array to string placeholder.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// Character Sets
 var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
 var selectedChars;
 var passwordLength;
 var usesNumbers;
@@ -95,11 +97,11 @@ function generatePassword() {
     selectedChars = uppercaseCharacters;
   }
 
-
-  var password = [];
-
+  // Array placeholder for user chosen length
+  var password = "";
   for (var i = 0; i < passwordLength; i++) {
-    var passwordText = selectedChars[Math.floor(Math.random() * selectedChars.length)];
-    password.push(passwordText.value);
+    var password = password + selectedChars[Math.floor(Math.random() * selectedChars.length)];
+    console.log(password);
   }
+  return password;
 } 
